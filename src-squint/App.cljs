2966f46ea39e38eb6c3940cf962b65d5b05524fc
Yogthos <yogthos@gmail.com>
2025-03-21 13:19:03 -0400

    add bulma

--- src-squint/App.cljs
+++ src-squint/App.cljs
@@ -6,26 +6,27 @@
 (defn ^:async greet [name set-greet-msg]
   (set-greet-msg (js-await (tauri/invoke "greet" {:name name}))))
 
-(defn App [] 
+(defn App []
   (let [[greet-msg set-greeting-msg] (hooks/useState "")
         [name set-name] (hooks/useState "")]
     #jsx [:main {:class "container"}
-          [:h1
-           "Welcome to Tauri + Preact"]
-          [:div {:class "row"}
-           [:a {:href "https://vitejs.dev" :target "_blank"}
-            [:img {:src "/vite.svg" :class "logo vite" :alt "Vite logo"}]]
-           [:a {:href "https://tauri.app" :target "_blank"}
-            [:img {:src "/tauri.svg" :class "logo tauri" :alt "Tauri logo"}]]]
-          [:p
-           "Click on the Tauri, Vite, and Preact logos to learn more."]
-          [:form {:class "row"
-                  :onSubmit (fn [e]
-                              (.preventDefault e)
-                              (greet name set-greeting-msg))}
-           [:input {:id "greet-input"
-                    :onInput (fn [e]
-                               (set-name (-> e .-currentTarget .-value)))
-                    :placeholder "Enter a name..."}]
-           [:button {:type "submit"} "Greet"]]
-          [:p greet-msg]]))
+          [:div {:class "p-6"}
+           [:h1 {:class "title"}
+            "Welcome to Tauri + Preact"]
+           [:div {:class "row"}
+            [:a {:href "https://vitejs.dev" :target "_blank"}
+             [:img {:src "/vite.svg" :class "logo vite" :alt "Vite logo"}]]]
+           [:p
+            "Click on the Tauri, Vite, and Preact logos to learn more."]
+           [:form {:onSubmit (fn [e]
+                               (.preventDefault e)
+                               (greet name set-greeting-msg))}
+            [:div {:class "field"}
+             [:input {:class "input"
+                      :id "greet-input"
+                      :onInput (fn [e]
+                                 (set-name (-> e .-currentTarget .-value)))
+                      :placeholder "Enter a name..."}]]
+            [:button {:class "button"
+                      :type "submit"} "Greet"]]
+           [:p greet-msg]]]))
